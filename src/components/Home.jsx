a4465d49ffd3d68de4bf600f3af513212f233f18
Cristian Vidal <cristiangvidal99@gmail.com>
2022-10-04 17:39:26 -0300

    Configuracion base

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routes from './Routes'
+
+const Home = () => {
+  return (
+    <>
+    
+    <Routes />
+    <div>Home</div>
+    </>
+  )
+}
+
+export default Home
